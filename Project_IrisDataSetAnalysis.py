1c4b12e1c459da3730960a8fa6f17de907510991
Aidan Conlon <ajconlon@gmail.com>
2019-04-17 21:38:39 +0100

    Removed some code and cleaned up the code somewhat. At this stage I am somewhat happy wit the plots for just the 1st flower type. I should be able to re-use all the code so far to create similar plots for the other type flowers and also differnet plots.

--- Project_IrisDataSetAnalysis.py
+++ Project_IrisDataSetAnalysis.py
@@ -3,50 +3,50 @@ This project is an analysis of the Fisher Iris Data Set that was published in 19
 
 Initially I would like to import the data set in order to work on it. '''
 
-import numpy as np       # Importing and anslyzing the data           
-import matplotlib.pyplot as plt # matplotlib for plotting the data
-import matplotlib.image as mpimg
-#from mpl_toolkits.mplot3d import Axes3D
-from sklearn.datasets import load_iris          #2 THis line of code retrieves the data set from scikit database hosting site
-iris = load_iris()                                #2 LEt iris equal to the load iris function
-#from sklearn.decomposition import PCA
+import numpy as np                      # Importing numpy for analyzing the data           
+import matplotlib.pyplot as plt         # Importing pyplot from matplotlib for plotting the data
+from sklearn.datasets import load_iris  #2 THis line of code retrieves the data set from scikit database hosting site
+iris = load_iris()                      #2 Let iris equal to the load iris function
+from PIL import Image                   # having to use the Python Imaging Library to import and display images in Python.                                                                             
+from statistics import mean             # import mean function from statistics
+
+
+def average(a, n):                       # Function that return average of an array to 2 decimal places 
+    sum = 0                                 # sum is 0 to begin with
+    for i in range(n):                      # for i is in the range of the elements (n) of the array
+        sum += a[i]                             # add each of the elements together as it progresses through the array
+    return round(sum/n, 2);                 # return a value (sum divided by the number of elements in the array) that is rounded to 2 decimal places, 
 
 
 
 
-from PIL import Image   # having to use the Python Imaging Library to import and display images in Python.                                                                             
-from statistics import mean # import mean function from statistics
 
+                                        # By using iris.data -> this only looks at the data arrays within the iris file taken from source.
+Iris_Setosa = (iris.data[0:50])         # Iris Setosa data is contained in the first 50 elements of the array 
+Iris_Versicolor = (iris.data[51:100])   # Iris Versicolor data is contained in the elements 50 - 100 of the array 
+Iris_Virginica = (iris.data[101:150])   # Iris Virginica data is contained in elements 100 - 150 of the array 
 
+                                        # I am just filling up an array with the specific data for each parameter associated with each breed 
+Sepal_Length = Iris_Setosa[:, 0]        # In this case I am setting Sepal_Length equal to each value of the array (all rows (of the 0-50), column 0)
+Sepal_Width = Iris_Setosa[:, 1]         # Here I am setting Sepal Width equal to each value of the array (all rows (of the 0 to 50), column 1)  
 
-
-
-#print(iris)                                     # Print Iris as a trstto ensure we are reading correctly.
-
-Iris_Setosa = (iris.data[0:50]) # By using iris.data -> this only looks at the data arrays within the iris file taken from source.
-Iris_Versicolor = (iris.data[51:100])
-Iris_Virginica = (iris.data[101:150])
-
-Sepal_Length = Iris_Setosa[:, 0]
-Sepal_Width = Iris_Setosa[:, 1]
-
-print(Sepal_Length)
-#print(Sepal_Width)
+                                        # As verification I wanto ensure I have extracted the data correctly so Iam printing it to sreen in these lines:
+print('The following figures are the values of the Sepal Length of the Iris Setosa flower:') # Print the text
+print(Sepal_Length,'\n\n')              # Print the values in the Sepal_Length array (50 values in total)...followed by 2 carriage returns                                                    
+print('The following figures are the values of the Sepal Width of the Iris Setosa flower:')  # Print the text
+print(Sepal_Width,'\n\n')               # Print the values in the Sepal_Width array (50 values in total)...followed by 2 carriage returns                                                    
 
 
 ########Show image as a intro:
-'''
+
 # Image saved from : https://i.ytimg.com/vi/ywIWUfjPCyY/maxresdefault.jpg
 img = Image.open('SepalPetal.jpg') # Open thse image from local machinre
 img.show()  # Show the image using default image viewer on local machine. 
-'''
+
 
 
 input("Press Enter to continue...")
 
-
-#pause()
-#imshow("SepalPetal.jpg")
 
 # Lets try and plot the data of Sepal Length against Width
 
@@ -60,32 +60,22 @@ plt.scatter(Iris_Setosa[:, 0], Iris_Setosa[:, 1], c="red", cmap=plt.cm.Set1, edg
 #plt.scatter(Iris_Versicolor[:, 0], Iris_Versicolor[:, 1], c="blue", cmap=plt.cm.Set1, edgecolor='k')
 #plt.scatter(Iris_Virginica[:, 0], Iris_Virginica[:, 1], c="green", cmap=plt.cm.Set1, edgecolor='k')
 
+
 # Need to determine the max value of each of the elements in the array
 
-
-print('Max ELement of Sepal Length is:', max(Iris_Setosa[:,0]))
-print('Min ELement of Sepal Length is:', min(Iris_Setosa[:,0]))
-
-def Average():
-    return mean() #sum(array) / len(array)
+print('\n')
+print('Max ELement of Sepal Length of Iris Setosa is:', max(Iris_Setosa[:,0])) # Print Max Value of Sepal Length of Iris Setosa 
+print('Min ELement of Sepal Length of Iris Setosa is:', min(Iris_Setosa[:,0])) # Print Min Value of Sepal Length of Iris Setosa 
 
 
-
-# Function that return average of an array to 2 decimal points. 
-def average(a, n): 
-    # Find sum of array element 
-    sum = 0
-    for i in range(n): 
-        sum += a[i] 
-    return round(sum/n, 2); 
-
-# Driver code 
 arr = (Iris_Setosa[:,0])
 n = len(arr) 
-print('The average of the Sepal Width is:', average(arr, n))
+print('The average of the Sepal Length of Iris Setosa is:', average(arr, n))
 
-
-
+# plot the average value on the scatter chart@: 
+plt.axvline(average(arr, n), color='m', linestyle='dashdot', linewidth=1)
+plt.axvline(max(Iris_Setosa[:,0]), color='r', linestyle='dashed', linewidth=1)
+plt.axvline(min(Iris_Setosa[:,0]), color='b', linestyle='dashed', linewidth=1)
 
 
 
@@ -97,81 +87,19 @@ print('The average of the Sepal Width is:', average(arr, n))
 #print('Average of the length is:', round(mean(Iris_Setosa[:,0])))
 
 
-print('Max ELement of Sepal Width is:', max(Iris_Setosa[:,1]))
-print('Min ELement of Sepal Width is:', min(Iris_Setosa[:,1]))
+print('Max ELement of Sepal Width of Iris Setosa is:', max(Iris_Setosa[:,1])) # Print Max value of Sepal Width of Iris Setosa
+print('Min ELement of Sepal Width of Iris Setosa is:', min(Iris_Setosa[:,1])) # Print Min value of Sepal Width of Iris Setosa
+arr = (Iris_Setosa[:,1])
+n = len(arr) 
+print('The average of the Sepal Width is:', average(arr, n))                  # Print the average value of Sepal Width for Iris Setosa 
+#Print the average or mean of the Y axis:
+plt.axhline(average(arr, n), color='m', linestyle='dashdot', linewidth=1)      # Plot the average value for the Sepal Width as a horizontal line, colour - m for magenta, dashed linestyle 
 
-#plt.clf()
-
-'''
-x = Iris_Setosa[:, :2]
-#X = Iris_Setosa[:, 0].min() -.5, X[:, 0].max() + 0.5
-y = iris.target
-
-
-plt.title('Some Sort of Graph for Display', fontsize=32)
-# Plot the training points
-#plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor='k')
-plt.xlabel('Sepal length', fontsize=18)
-plt.ylabel('Sepal width', fontsize=18)
-
-'''
+# I would like now to have on the one graph the max min values on both axes so that it is easily spoken about.
+plt.axhline(max(Iris_Setosa[:,1]), color='r', linestyle='dashed', linewidth=1)
+plt.axhline(min(Iris_Setosa[:,1]), color='b', linestyle='dashed', linewidth=1)
 
 
-
-#plt.clf()
-
-'''
-print(Iris_Setosa)  # test just to ensure I have extracted correct data points
-print(Iris_Versicolor) # test just to ensure I have extracted correct data points
-print(Iris_Versicolor) # test just to ensure I have extracted correct data points
-#
-'''
-# Iris_Setosa = 
-
-#data_file = np.loadtxt('data_file.txt', delimiter=',')       #1   
-
-#sample_number = data_file[:,0]                              #1
-
-
-'''
-#iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features.
-y = iris.target
-
-x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-
-plt.figure(1, figsize=(12, 12)) # FIgsize is the size of the plot in inches
-plt.clf()
-
-plt.title('Some Sort of Graph for Display', fontsize=32)
-# Plot the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1, edgecolor='k')
-plt.xlabel('Sepal length', fontsize=18)
-plt.ylabel('Sepal width', fontsize=18)
-
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
-plt.grid(True)
-#plt.grid(color='r', linestyle='-', linewidth=2)
-
-'''
-'''
-fig = plt.figure(1, figsize=(8, 6))
-ax = Axes3D(fig, elev=-150, azim=110)
-X_reduced = PCA(n_components=3).fit_transform(iris.data)
-ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y,
-           cmap=plt.cm.Set1, edgecolor='k', s=40)
-ax.set_title("First three PCA directions")
-ax.set_xlabel("1st eigenvector")
-ax.w_xaxis.set_ticklabels([])
-ax.set_ylabel("2nd eigenvector")
-ax.w_yaxis.set_ticklabels([])
-ax.set_zlabel("3rd eigenvector")
-ax.w_zaxis.set_ticklabels([])
-'''
 plt.show()
 
 # 1 https://apmonitor.com/che263/index.php/Main/PythonDataAnalysis
@@ -179,52 +107,3 @@ plt.show()
 
 
 
-'''
-
-THis all works to show the plot in a  3 dmentional plot. 
-
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn import datasets
-from sklearn.decomposition import PCA
-
-# import some data to play with
-iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features.
-y = iris.target
-
-x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-
-plt.figure(2, figsize=(8, 6))
-plt.clf()
-
-# Plot the training points
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Set1,
-            edgecolor='k')
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
-
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
-
-# To getter a better understanding of interaction of the dimensions
-# plot the first three PCA dimensions
-fig = plt.figure(1, figsize=(8, 6))
-ax = Axes3D(fig, elev=-150, azim=110)
-X_reduced = PCA(n_components=3).fit_transform(iris.data)
-ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y,
-           cmap=plt.cm.Set1, edgecolor='k', s=40)
-ax.set_title("First three PCA directions")
-ax.set_xlabel("1st eigenvector")
-ax.w_xaxis.set_ticklabels([])
-ax.set_ylabel("2nd eigenvector")
-ax.w_yaxis.set_ticklabels([])
-ax.set_zlabel("3rd eigenvector")
-ax.w_zaxis.set_ticklabels([])
-
-plt.show()
-'''
